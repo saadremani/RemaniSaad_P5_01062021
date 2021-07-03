7f3bfc036468c1d18912f8c62040f53bce4458e8
REMANI_Saad <remani.islam2014@gmail.com>
2021-07-03 14:19:41 +0200

    up_to_dev_branch

--- Orinoco/front-end/JS/app.js
+++ Orinoco/front-end/JS/app.js
@@ -1,2 +1,7 @@
 let elt = document.getElementById('main');
 elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
+
+elt.classList.add("nouvelleClasse");    // Ajoute la classe nouvelleClasse à l'élément
+elt.classList.remove("nouvelleClasse"); // Supprime la classe nouvelleClasse que l'on venait d'ajouter
+elt.classList.contains("nouvelleClasse");   // Retournera false car on vient de la supprimer
+elt.classList.replace("oldClass", "newClass"): // Remplacera oldClass par newClass si oldClass était présente sur l'élément
